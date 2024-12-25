488a1a731ff30a65435f3d305eb187533223f07b
Sadaf K. <sadaf.kp81@gmail.com>
2024-12-25 12:32:23 +0330

    baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Shopproject/';
+$config['base_url'] = 'https://b8da-84-47-210-12.ngrok-free.app/';
 
 /*
 |--------------------------------------------------------------------------
